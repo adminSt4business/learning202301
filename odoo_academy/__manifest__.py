ccf9108d32fe37f103c93f151e3dbcf40c068efc
Javier Prieto <jprieto@st4business.com>
2023-01-25 04:33:48 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8  -*-
+
+{'name': 'Odoo Academy',
+ 'sumary': """Academy app to manage Training...""",
+ 'description': """
+    Academy Module to manage training:
+    - Courses
+    - Session
+    - Attendees
+ """,
+ 'author': 'JPrieto',
+ 'website': 'www.st4business.com',
+ 'category': 'Training',
+ 'version': '0.1',
+ 'depends': ['base'],
+ 'data': [],
+ 'demo': []
+}
